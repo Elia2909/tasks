43d8dd85311fba40582fcddf48c5b82d431b140c
illiasytnik <ilia.sutnik@customertimes.com>
2022-10-06 15:35:42 +0300

    add task one

--- tasks/src/main/java/StartProgram.java
+++ tasks/src/main/java/StartProgram.java
@@ -1,5 +1,20 @@
 public class StartProgram {
     public static void main(String[] args) {
+        System.out.println(tripleTrouble("aa", "bb", "cc"));
+        System.out.println(tripleTrouble("LLh", "euo", "xtr"));
+    }
+    /*
+    * Triple Trouble
+    Create a function that will return a string that combines all of the letters of the three
+    inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other.
+    Do this for every letter, see example below!
 
+    E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+    Note: You can expect all of the inputs to be the same length.
+*/
+    public static String tripleTrouble(String one, String two, String three) {
+        // Solution
+        return null;
     }
 }
